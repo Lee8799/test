c703e4d47a8a6797287fad63cc6ff665dce7bb60
Cynthia Varelas <cynthiavarelas@Cynthias-iMac.local>
2021-06-28 16:16:21 -0700

    completed

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+    return (
+        <div className="Home">
+          <div className="lander">
+            <h1>Fitness Trackr</h1>
+            <p>A performance tracking app for Fitness Junkies</p>
+          </div>
+          <div>
+
+          </div>
+        </div>
+      );
+    }
+
+export default Home;
